642db6e4ad5ec2f9ee30878ba7c88ab640194dfa
Isai <jeanmaryisai@gmail.com>
2022-08-16 22:47:06 -0400

    "1:23:12"

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -6,18 +6,40 @@ import com.example.demo.repo.RoleRepo;
 import com.example.demo.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService, UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user= userRepo.findByUsername(username);
+        if(user== null){
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found");
+        } else {
+            log.info("User found");
+        }
+        Collection<SimpleGrantedAuthority> authorities=new ArrayList<>();
+        user.getRoles().forEach(role -> {authorities.add(new SimpleGrantedAuthority(role.getName()));});
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(),authorities);
+    }
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+    private final PasswordEncoder passwordEncoder;
     @Override
     public User saveUser(User user) {
         log.info("Saving user to the database");
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 
@@ -46,4 +68,6 @@ public class UserServiceImpl implements UserService{
         log.info("Get all users");
         return userRepo.findAll();
     }
+
+
 }
