703e644dc8f6e1cf982c5c86c87003b04b7c073b
Isai <jeanmaryisai@gmail.com>
2022-08-17 01:43:14 -0400

    last commit

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.security;
 
 import com.example.demo.filter.CustomAuthentificationFilter;
+import com.example.demo.filter.CustomAuthorisationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration @EnableWebSecurity @RequiredArgsConstructor
@@ -22,10 +26,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthentificationFilter customAuthentificationFilter=new CustomAuthentificationFilter(authenticationManagerBean());
+
+        //setup customlogin path
+        customAuthentificationFilter.setFilterProcessesUrl("/api/login");
+
+
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new CustomAuthentificationFilter(authenticationManagerBean()));
+        http.authorizeRequests().antMatchers("/api/login/**","/api/token/refresh/**").permitAll();
+
+        http.authorizeRequests().antMatchers(GET,"/api/user/**").hasAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(POST,"/api/user/save**").hasAuthority("ROLE_ADMIN");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(customAuthentificationFilter);
+        http.addFilterBefore(new CustomAuthorisationFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
 
